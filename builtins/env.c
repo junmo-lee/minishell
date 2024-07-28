03f66908d80db0dbd19b8f94b4df74bd329e69fc
choolee <saltey12848@gmail.com>
2024-07-28 17:21:20 +0900

    export 할때 밸류값이 널인경우 env를 하면 세그 뜨는거 수정

--- builtins/env.c
+++ builtins/env.c
@@ -14,6 +14,11 @@ int	env(t_parser_list *cmd_list, t_envp_list **envp_list)
 	{
 		while (current_node != NULL)
 		{
+			if (current_node->value == NULL)
+			{
+				current_node = current_node->next;
+				continue ;
+			}
 			write (1, current_node->key, ft_strlen(current_node->key));
 			write (1, "=", 1);
 			write (1, current_node->value, ft_strlen(current_node->value));
