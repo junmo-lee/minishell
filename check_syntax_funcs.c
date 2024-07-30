b54c0258bc9ec317a81f84b6317a91d61efd4af1
junmo-lee <junmlee@student.42seoul.kr>
2024-07-30 15:33:41 +0900

    cmd line에 string 이 없더라도 PIPE_ERROR 가 발생하지 않게 변경

--- check_syntax_funcs.c
+++ check_syntax_funcs.c
@@ -29,7 +29,9 @@ static void	check_pipe_syntax(t_info *info_s, t_parser_list *parser_list)
 		if (current_node->type == PIPE)
 		{
 			if (prev_type != STRING || current_node->next == NULL \
-			|| current_node->next->type != STRING)
+			|| !(current_node->next->type == STRING \
+			|| current_node->next->type == HERE_DOC \
+			|| current_node->next->type == REDIRECTION))
 				info_s->error = PIPE_ERROR;
 		}
 		prev_type = current_node->type;
