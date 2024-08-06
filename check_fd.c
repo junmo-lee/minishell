3c1c958bd7990c36932bbf374fb72e25d85a7092
junmo-lee <junmlee@student.42seoul.kr>
2024-08-06 18:04:17 +0900

    check_fd pipe 확인 추가

--- check_fd.c
+++ check_fd.c
@@ -2,7 +2,24 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <libproc.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
+int is_pipe(int fd) {
+    struct stat statbuf;
+    if (fstat(fd, &statbuf) == -1) {
+        perror("fstat");
+        return -1;
+    }
+
+    // 파일 타입이 FIFO(파이프)인지 확인
+    if (S_ISFIFO(statbuf.st_mode)) {
+        return 1;  // 파이프
+    } else {
+        return 0;  // 파이프 아님
+    }
+}
 
 int check_fd(char *str) 
 {
@@ -29,9 +46,24 @@ int check_fd(char *str)
     
     int numberOfFDs = bufferSize / PROC_PIDLISTFD_SIZE;
     
-    fprintf(stderr, "%s(%5d): ", str, pid);
-    for (int i = 0; i < numberOfFDs; i++) {
-        fprintf(stderr, "%d ", fds[i].proc_fd);
+    fprintf(stderr, "%s(%5d):\n", str, pid);
+    for (int i = 0; i < numberOfFDs; i++) 
+    {
+        if (i == 2)
+            continue;
+        fprintf(stderr, "%d : ", fds[i].proc_fd);
+        if (is_pipe(fds[i].proc_fd))
+        {
+            fprintf(stderr, "is pipe\n");
+        }
+        else
+        {
+            char path[1024];
+            if (fcntl(fds[i].proc_fd, F_GETPATH, path) != -1) 
+            {
+                fprintf(stderr, "File path: %s\n", path);
+            }
+        }
     }
 	fprintf(stderr, "\n");
     
